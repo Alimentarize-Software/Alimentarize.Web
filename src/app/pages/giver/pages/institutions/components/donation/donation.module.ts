faac5473f687cc168deeb15ce1dbfbc8e3a1c783
BeatrizOSantos <beatrizodsk@gmail.com>
2024-05-06 11:47:03 -0300

    feat: continuação do layout base de doações

--- src/app/pages/giver/pages/institutions/components/donation/donation.module.ts
+++ src/app/pages/giver/pages/institutions/components/donation/donation.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DonationComponent } from './donation.component';
 import { ButtonModule } from 'src/app/shared/components/button/button.module';
 import { DonationRoutingModule } from './donation-routing.module';
+import { InputModule } from "../../../../../../shared/components/input/input.module";
 
 
 
 @NgModule({
-  declarations: [DonationComponent],
-  imports: [
-    CommonModule, DonationRoutingModule, ButtonModule
-  ],
-  exports: [DonationComponent],
+    declarations: [DonationComponent],
+    exports: [DonationComponent],
+    imports: [
+        CommonModule, DonationRoutingModule, ButtonModule,
+        InputModule
+    ]
 })
 export class DonationModule { }
