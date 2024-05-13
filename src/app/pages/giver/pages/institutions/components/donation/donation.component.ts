14cb2cbe591f44321e782b63e356086a416bd713
BeatrizOSantos <beatrizodsk@gmail.com>
2024-05-12 21:51:27 -0300

    feat: implementação de mock de status e tela de doação concluída

--- src/app/pages/giver/pages/institutions/components/donation/donation.component.ts
+++ src/app/pages/giver/pages/institutions/components/donation/donation.component.ts
@@ -7,8 +7,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./donation.component.sass']
 })
 export class DonationComponent {
+  filterOptions: any[] = [
+    { id: 'option1', label: 'Arroz', status: 'urgente' },
+    { id: 'option2', label: 'Feijão', status: 'moderado' },
+    { id: 'option3', label: 'Macarrão', status: 'nao_urgencia' }
+  ];
+  donationCompleted: boolean = false;
   
   constructor(private router: Router) {}
 
-  
+  deleteFoodItem() {
+    console.log('deleteFoodItem')
+  }
+
+  finalizeDonation() {
+    console.log('finalizeDonation')
+    this.donationCompleted = true;
+  }
+
+  getSvgColor(status: string): string {
+    switch (status) {
+      case 'urgente':
+        return '#A9320C';
+      case 'moderado':
+        return '#FFC107';
+      case 'nao_urgencia':
+        return '#4CAF50';
+      default:
+        return '';
+    }
+  }
 }
