f4cffd4f28335bfe5901eafd7d2f18ee7a0ae402
BeatrizOSantos <beatrizodsk@gmail.com>
2024-05-20 01:09:48 -0300

    ajuste header

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.sass']
 })
 export class MainComponent {
+  activePath: string;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activePath = this.getActiveRootPath(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.activePath = this.getActiveRootPath(this.router.url);
+  }
+
+  private getActiveRootPath(url: string): string {
+    const segments = url.split('/');
+    return '/' + segments[1] + (segments[2] ? '/' + segments[2] : '');
+  }
+
+  public checkHeader() {
+    return !(
+      this.activePath === '/doador/meu-perfil' ||
+      this.activePath === '/instituicao/meu-perfil' ||
+      this.activePath === '/doador/configuracoes' ||
+      this.activePath === '/instituicao/configuracoes'
+    );
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,32 +6,6 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
-  activePath: string;
-
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.activePath = this.getActiveRootPath(event.urlAfterRedirects);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    this.activePath = this.getActiveRootPath(this.router.url);
-  }
-
-  private getActiveRootPath(url: string): string {
-    const segments = url.split('/');
-    return '/' + segments[1] + (segments[2] ? '/' + segments[2] : '');
-  }
-
-  public checkHeader() {
-    return !(
-      this.activePath === '/doador/meu-perfil' ||
-      this.activePath === '/instituicao/meu-perfil' ||
-      this.activePath === '/doador/configuracoes' ||
-      this.activePath === '/instituicao/configuracoes'
-    );
-  }
+  
 
 }
