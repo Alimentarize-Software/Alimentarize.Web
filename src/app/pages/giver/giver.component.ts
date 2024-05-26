c689a21acbc5cca63caa21a0ccdfdc36003a859e
Nicholas <nicholas.nogueira@gocase.com.br>
2024-05-26 11:30:08 -0300

    fix: carousel responsive weird behavior

--- src/app/pages/giver/giver.component.ts
+++ src/app/pages/giver/giver.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { GiverService } from './giver.service';
 import {
   Institution,
@@ -8,7 +8,9 @@ import {
   HistoryDonation,
   HistoryDonationResponse,
 } from 'src/app/core/model/historyDonation';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { OwlOptions, CarouselComponent } from 'ngx-owl-carousel-o';
+
+declare var $: any;
 
 @Component({
   selector: 'app-giver',
@@ -16,6 +18,7 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./giver.component.sass'],
 })
 export class GiverComponent implements OnInit {
+  @ViewChild('owlCarousel', { static: false }) owlCarousel: CarouselComponent;
   customOptions: OwlOptions = {
     mouseDrag: false,
     touchDrag: false,
@@ -41,7 +44,7 @@ export class GiverComponent implements OnInit {
   
   skeletons: any[] = new Array(8);
 
-  constructor(private giverService: GiverService) {}
+  constructor(private giverService: GiverService, private renderer: Renderer2) {}
   donationHistory: HistoryDonationResponse = {} as HistoryDonationResponse;
   ngOnInit(): void {
     const object = localStorage.getItem('user');
@@ -50,9 +53,19 @@ export class GiverComponent implements OnInit {
       this.giverService.getLatestInstitution(user?.id, 1, 10).subscribe({
         next: (data: HistoryDonationResponse) => {
           this.donationHistory = data;
-          console.log('donationHistory: ', this.donationHistory);
+          // console.log('donationHistory: ', this.donationHistory);
         },
       });
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.listen('document', 'DOMContentLoaded', () => {
+      console.log('ready');
+    });
+    console.log('owlCarousel: ', this.owlCarousel);
+    if (this.owlCarousel) {
+      // this.owlCarousel.refresh();
     }
   }
 }
